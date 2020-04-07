38a49f83dae3a204a482dcc210c29c0084b06844
Maxim <financial_monster@mail.ru>
2020-04-07 17:53:55 +0400

    Add snapshot and e2e tests of the Card component

--- src/components/card/card.e2e.test.js
+++ src/components/card/card.e2e.test.js
@@ -4,3 +4,50 @@ import Adapter from 'enzyme-adapter-react-16';
 import Card from './card.jsx';
 
 Enzyme.configure({adapter: new Adapter()});
+
+const offer =  {
+    price: 80,
+    isPinned: true,
+    isPremium: false,
+    rating: 4,
+    title: `Wood and stone place`,
+    type: `Private room`,
+    src: `img/room.jpg`,
+    id: 1
+};
+
+describe(`Interactions with user triggers appropriate callbacks with right values`, () => {
+    it(`When mouse starts to be over the component, corresponding mouseenter callback is triggered`, () => {
+        const mouseEnterHandler = jest.fn();
+        const card = shallow(<Card offer={offer} onMouseEnter={mouseEnterHandler}
+            onMouseLeave={jest.fn()} isHovered={false} />);
+        const article = card.find(`article`);
+
+        article.simulate(`mouseenter`);
+
+        expect(mouseEnterHandler).toHaveBeenCalledTimes(1);
+    });
+
+    it(`When mouse starts to be over the component, corresponding mouseenter callback is triggered with the right
+        value`, () => {
+        const mouseEnterHandler = jest.fn();
+        const card = shallow(<Card offer={offer} onMouseEnter={mouseEnterHandler}
+            onMouseLeave={jest.fn()} isHovered={false} />);
+        const article = card.find(`article`);
+
+        article.simulate(`mouseenter`);
+
+        expect(mouseEnterHandler).toHaveBeenNthCalledWith(1, offer.id);
+    });
+
+    it(`When mouse ends to be over the component, corresponding mouseleave callback is triggered`, () => {
+        const mouseLeaveHandler = jest.fn();
+        const card = shallow(<Card offer={offer} onMouseEnter={jest.fn()}
+            onMouseLeave={mouseLeaveHandler} isHovered={false} />);
+        const article = card.find(`article`);
+
+        article.simulate(`mouseleave`);
+
+        expect(mouseLeaveHandler).toHaveBeenCalledTimes(1);
+    });
+});
--- src/components/card/card.test.js
+++ src/components/card/card.test.js
@@ -1,3 +1,71 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+
 import Card from './card.jsx';
+
+const offers = [
+    {
+        price: 120,
+        isPinned: false,
+        isPremium: true,
+        rating: 4,
+        title: `Beautiful luxurious apartment at great location`,
+        type: `Apartment`,
+        src: `img/apartment-01.jpg`,
+        id: 0
+    },
+    {
+        price: 80,
+        isPinned: true,
+        isPremium: false,
+        rating: 4,
+        title: `Wood and stone place`,
+        type: `Private room`,
+        src: `img/room.jpg`,
+        id: 1
+    },
+    {
+        price: 132,
+        isPinned: false,
+        isPremium: false,
+        rating: 4,
+        title: `Canal View Prinsengracht`,
+        type: `Apartment`,
+        src: `img/apartment-02.jpg`,
+        id: 2
+    }
+]
+
+describe(`Correctly renders`, () => {
+    it(`Correctly renders Card component which is not pinned, premium or hovered`, () => {
+        const tree = renderer.create(<Card offer={offers[2]} onMouseEnter={jest.fn()}
+            onMouseLeave={jest.fn()} isHovered={false}/>)
+            .toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it(`Correctly renders Card component which is not pinned, premium but hovered`, () => {
+        const tree = renderer.create(<Card offer={offers[2]} onMouseEnter={jest.fn()}
+            onMouseLeave={jest.fn()} isHovered={true}/>)
+            .toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });   
+    
+    it(`Correctly renders Card component which is not pinned, hovered but premium`, () => {
+        const tree = renderer.create(<Card offer={offers[0]} onMouseEnter={jest.fn()}
+            onMouseLeave={jest.fn()} isHovered={false}/>)
+            .toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it(`Correctly renders Card component which is not premium, hovered but pinned`, () => {
+        const tree = renderer.create(<Card offer={offers[1]} onMouseEnter={jest.fn()}
+            onMouseLeave={jest.fn()} isHovered={false}/>)
+            .toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+});
