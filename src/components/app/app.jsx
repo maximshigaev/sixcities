50acdf3020daa9725422c095535e8b52766cdd0d
Maxim <financial_monster@mail.ru>
2020-04-07 20:38:37 +0400

    Add routing to the App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 
-import MainPage from '../pages/main.jsx'; 
+import MainPage from '../pages/mainPage.jsx';
+import LoginPage from '../pages/loginPage.jsx';
+import FavoritesPage from '../pages/favoritesPage.jsx';
+import OfferPage from '../pages/offerPage.jsx';
 
 const App = () => {
 	return (
-		<MainPage />
+		<Router>
+			<Switch>
+				<Route path="/" component={MainPage} exact/>
+				<Route path="/login" component={LoginPage} exact/>
+				<Route path="/favorites" component={FavoritesPage} exact/>
+				<Route path="/offer/:id" component={OfferPage} exact/>
+				<Redirect to="/" />
+			</Switch>
+		</Router>
 	);
 }
 
