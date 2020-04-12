babadf079083a15096e293d37a737945ecf598e1
Maxim <financial_monster@mail.ru>
2020-04-12 21:01:42 +0400

    Updates

---
+++ precache-manifest.eb26310368bde87f5490d44eb7a866ff.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7da5b642ef51141ffabb06911ba6f22",
+    "url": "/sixcities/index.html"
+  },
+  {
+    "revision": "deac08c2c89e13fa148b",
+    "url": "/sixcities/static/css/main.95c2bc6c.chunk.css"
+  },
+  {
+    "revision": "416642d069e28e009733",
+    "url": "/sixcities/static/js/2.1fb4c816.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/sixcities/static/js/2.1fb4c816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deac08c2c89e13fa148b",
+    "url": "/sixcities/static/js/main.f21613c0.chunk.js"
+  },
+  {
+    "revision": "339044ea9e4ca4797b3b",
+    "url": "/sixcities/static/js/runtime-main.c2599022.js"
+  },
+  {
+    "revision": "052bc6299ca98e2f8c076321a5e9b4df",
+    "url": "/sixcities/static/media/logo.052bc629.svg"
+  },
+  {
+    "revision": "6747ef6e37bd2ba7da8d9ca6a2082442",
+    "url": "/sixcities/static/media/pin-active.6747ef6e.svg"
+  },
+  {
+    "revision": "19314a0f1607ce0e42b3758677b9caba",
+    "url": "/sixcities/static/media/pin.19314a0f.svg"
+  }
+]);
