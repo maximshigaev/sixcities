e5cdf053398c12a93162baf9aa8a58da5d608285
Maxim <financial_monster@mail.ru>
2020-04-20 19:30:01 +0400

    Add snapshot test of the nearbyHotels component

---
+++ src/components/nearbyHotels/nearbyHotels.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import {BrowserRouter as Router} from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import {Provider} from 'react-redux';
+
+import NearbyHotels from './nearbyHotels.jsx';
+
+const hotels =  [{price: 120, is_premium: true, is_favorite: false, rating: 4, src: `img/apartment-01.jpg`,
+    title: `Beautiful luxurious apartment at great location`, type: `Apartment`,
+    preview_image: `https://htmlacademy-react-3.appspot.com/six-cities/static/hotel/9.jpg`, id: 1},
+    {price: 100, is_premium: false, is_favorite: true, rating: 5, src: `img/apartment-02.jpg`,
+    title: `Perfectly located Castro`, type: `Room`,
+    preview_image: `https://htmlacademy-react-3.appspot.com/six-cities/static/hotel/4.jpg`, id: 2},
+    {price: 80, is_premium: true, is_favorite: true, rating: 3, src: `img/apartment-03.jpg`,
+    title: `Waterfront with extraordinary view`, type: `Hotel`,
+    preview_image: `https://htmlacademy-react-3.appspot.com/six-cities/static/hotel/3.jpg`, id: 3}
+];
+
+it(`Correctly renders the NearbyHotels component`, () => {
+    const mockStore = configureStore([]);
+    const store = mockStore();
+
+    const tree = renderer.create(
+        <Provider store={store}>
+            <Router>
+                <NearbyHotels hotels={hotels} />
+            </Router>
+        </Provider>
+    );
+
+    expect(tree).toMatchSnapshot();
+});
