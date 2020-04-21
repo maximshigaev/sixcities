22bdefa1b8dd4f76e41635c3ce9e6a5be08217b6
Maxim <financial_monster@mail.ru>
2020-04-21 13:52:35 +0400

    Update snapshot test of the mainPage component

--- src/components/pages/mainPage/mainPage.test.js
+++ src/components/pages/mainPage/mainPage.test.js
@@ -1,11 +1,68 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
+import thunk from 'redux-thunk';
 
+import api from '../../../api.js';
 import {MainPage} from './mainPage.jsx';
 
-it(`Correctly renders the MainPage component `, () => {
-    const tree = renderer.create(<MainPage />)
+describe(`Correctly renders the MainPage component `, () => {
+    it(`when isLoading property is equal true and hasOffers property is equal false`, () => {
+        const mockStore = configureStore([thunk.withExtraArgument(api)]);
+        const store = mockStore({
+            offers: {offers: [], isLoading: true, isError: false, currentSorting: `Popular`},
+            card: {focusedCard: null},
+            auth: {isLoggedIn: false, isAuthStatusLoading: false , isAuthStatusError: false}
+        });
+
+        const tree = renderer.create(
+            <Provider store={store}>
+                <Router>
+                    <MainPage isLoading={true} hasOffers={false} />
+                </Router>
+            </Provider>)
         .toJSON();
 
-    expect(tree).toMatchSnapshot();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it(`when isLoading property is equal false and hasOffers property is equal true`, () => {
+        const mockStore = configureStore([thunk.withExtraArgument(api)]);
+        const store = mockStore({
+            offers: {offers: [], isLoading: true, isError: false, currentSorting: `Popular`},
+            card: {focusedCard: null, isLoading: false, isError: false},
+            auth: {isLoggedIn: false, isAuthStatusLoading: false , isAuthStatusError: false}
+        });
+
+        const tree = renderer.create(
+            <Provider store={store}>
+                <Router>
+                    <MainPage isLoading={false} hasOffers={true} />
+                </Router>
+            </Provider>)
+        .toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+
+     it(`when both hasOffers and isLoading properties are equal false`, () => {
+        const mockStore = configureStore([thunk.withExtraArgument(api)]);
+        const store = mockStore({
+            offers: {offers: [], isLoading: false, isError: false, currentSorting: `Popular`},
+            card: {focusedCard: null},
+            auth: {isLoggedIn: false, isAuthStatusLoading: false , isAuthStatusError: false}
+        });
+
+        const tree = renderer.create(
+            <Provider store={store}>
+                <Router>
+                    <MainPage isLoading={false} hasOffers={false} />
+                </Router>
+            </Provider>)
+        .toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
 });
